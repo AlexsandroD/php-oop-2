db4bc07555ae094f02a2255f1d45623670ca5be2
AlexsandroD <adrdra88@gmail.com>
2022-01-31 15:55:06 +0100

    prova 2

--- classes/utenti_registrati.php
+++ classes/utenti_registrati.php
@@ -2,19 +2,19 @@
 
 class Cc {
     private $cc;
-
+    
     public function getCc($int){
     $this->cc = $int;
     }
 }
-
-class Utente extends Cc{
+    
+    class Utente extends Cc{
         public $first_name;
         public $last_name;
         public $membership;
         private $cc;
-
-        public function __construct($_first_name,$_last_name,$_membership){
+        
+        public function __construct($_first_name,$_last_name,$_membership,$_cc){
             $this->first_name = $_first_name;
             $this->last_name = $_last_name;
             $this->membership = $_membership;
