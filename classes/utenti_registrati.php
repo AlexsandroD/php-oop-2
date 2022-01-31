7074790d7a61b02cc244eddb97c7f742f29bfa09
AlexsandroD <adrdra88@gmail.com>
2022-01-31 17:19:47 +0100

    nonHoCapito

--- classes/utenti_registrati.php
+++ classes/utenti_registrati.php
@@ -1,25 +1,33 @@
 <?php
-
 class Cc {
-    private $getCc();
-    
-    public function getCc($int){
+    private $cc;
+
+    public function setCc($int){
     $this->cc = $int;
     }
 }
+
     
-    class Utente extends Cc{
+    class Utente extends Cc {
         public $first_name;
         public $last_name;
         public $membership;
-        private $cc;
+        public $cc;
+        public $products = array();
+
         
         public function __construct($_first_name,$_last_name,$_membership,$_cc){
             $this->first_name = $_first_name;
             $this->last_name = $_last_name;
             $this->membership = $_membership;
-            $this->cc = $_cc;
+            $this->setCc($_cc);
         }
+       
+        public function addProduct(){
+            $this->products.array_push($this->Products);        }
+
     }
+    var_dump($products)
+
 
 ?>
--- index.php
+++ index.php
@@ -9,11 +9,9 @@ Gestite eventuali eccezioni che si possono verificare (es: carta di credito scad
 <?php
     require_once __DIR__ . '/classes/product_list.php';
     require_once __DIR__ . '/classes/utenti_registrati.php';
-
-    
-  
+ 
     $Utenti = [
-        new Utente('Marco','Argento','Standard'),
+        new Utente('Marco','Argento','Standard',1234),
   
     ];
 
